ea3a303922d6004461bea2f3ec09b9ad426207d9
Steven Gangano <stevengangano@Stevens-MacBook-Pro.local>
2016-09-21 19:55:17 -0700

    first commit

---
+++ js/controller.js
@@ -0,0 +1,36 @@
+var myApp=angular.module('myApp',['ngRoute'])
+
+myApp.config(function ($routeProvider) {
+
+	$routeProvider
+
+	.when ('/', {
+		templateUrl: 'pages/home.html'	
+	})
+
+	.when ('/about', {
+		templateUrl: 'pages/about.html'
+		// controller: '2015videoGameCtrl'
+	})
+
+	.when ('/catering', {
+		templateUrl: 'pages/catering.html'
+		// controller: '2014videoGameCtrl'
+	})
+
+	.when ('/contact', {
+		templateUrl: 'pages/contact.html'
+		// controller: '2015videoGameCtrl'
+	})
+
+	.when ('/home', {
+		templateUrl: 'pages/home.html'
+		// controller: '2015videoGameCtrl'
+	})
+
+	.when ('/menu', {
+		templateUrl: 'pages/menu.html'
+		// controller: '2015videoGameCtrl'
+	})
+})
+
