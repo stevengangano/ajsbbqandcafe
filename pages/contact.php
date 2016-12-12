071220ca77a0b464d943bf63e502217451ce3cfa
Steven Gangano <stevengangano@Stevens-MacBook-Pro.local>
2016-12-11 23:04:05 -0800

    updated

--- pages/contact.php
+++ pages/contact.php
@@ -41,127 +41,82 @@
   </nav>
 </header>
     
+<div class="row">
 
-<!-- Carousel -->
-<div class="container">
-  <div class="row slider">
-    <div class="col-xs-12 col-sm-12 col-md-8 col-md-push-2 col-lg-10 col-lg-push-1 nopadding">
-      <div id="myCarousel" class="carousel slide text-center" data-ride="carousel" data-interval="3000">
-        <!-- Indicators -->
-        <ol class="carousel-indicators text-center">
-          <li data-target="#myCarousel" data-slide-to="0" class="active text-center"></li>
-          <li data-target="#myCarousel" data-slide-to="1"></li>
-          <li data-target="#myCarousel" data-slide-to="2"></li>
-          <li data-target="#myCarousel" data-slide-to="3"></li>
-        </ol>
+  <div class="col-lg-8 col-md-8 col-sm-12 col-xs-12">
 
-        <!-- Wrapper for slides -->
-        <div class="carousel-inner" role="listbox">
+    <h1> Contact </h1>
 
-          <div class="item home active">
-            <img src="https://www.maxschicken.com/administrator/public/uploads/banners/c6d0aa8ff7380689f5bd9e13a447aaa8.jpg" alt="Chania">
-          </div>
+    <hr class="hr">
 
-          <div class="item home">
-            <img src="https://www.maxschicken.com/administrator/public/uploads/banners/96ebdd45abc348a4fbc064beb6b2eae3.jpg" alt="Chania">
-          </div>
+     <h6 class="featured-events-news-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+            quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+            consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+            cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+            proident, sunt in culpa qui officia deserunt mollit anim id est laborum. 
+      </h6>
+      <h6 class="featured-events-news-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+            quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+            consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+            cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+            proident, sunt in culpa qui officia deserunt mollit anim id est laborum. 
+      </h6>
+      <h6 class="featured-events-news-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+            quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+            consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+            cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+            proident, sunt in culpa qui officia deserunt mollit anim id est laborum. 
+      </h6>
+      <h6 class="featured-events-news-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+            quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+            consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+            cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+            proident, sunt in culpa qui officia deserunt mollit anim id est laborum. 
+      </h6>
+      <h6 class="featured-events-news-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+            quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+            consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+            cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+            proident, sunt in culpa qui officia deserunt mollit anim id est laborum. 
+      </h6>
+      <h6 class="featured-events-news-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+            quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+            consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+            cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+            proident, sunt in culpa qui officia deserunt mollit anim id est laborum. 
+      </h6>
+      <h6 class="featured-events-news-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+            quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+            consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+            cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+            proident, sunt in culpa qui officia deserunt mollit anim id est laborum. 
+      </h6>
 
-          <div class="item home">
-            <img src="https://www.maxschicken.com/administrator/public/uploads/banners/8dc581e1717dbf079655d1715dee08e1.jpg" alt="Flower">
-          </div>
 
-          <div class="item home">
-            <img src="https://www.maxschicken.com/administrator/public/uploads/banners/5b3153ad5c7603670b97925097ee6f4f.jpg" alt="Flower">
-          </div>
+  </div> <!-- end col -->
 
-        </div> <!-- end carousel-inner -->
-
-        <!-- Controls -->
-  <a class="left carousel-control" href="#myCarousel"  role="button" data-slide="prev">
-    <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
-    <span class="sr-only">Previous</span>
-  </a>
-  <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
-    <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
-    <span class="sr-only">Next</span>
-  </a>
+    <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12">
+      <div class = "panel-body">
+        <img src="img/bbq.jpg" class="img-thumbnail about-photo">
       </div>
-    </div>
   </div>
-</div>
 
-<br>
+</div> <!-- end row -->
 
-<div class="container">
-  <div class="row">
-    <div class="col-lg-8 col-md-8 col-xs-12 col-xs-12">
-
-      <div id="wrapper" style="padding-left:12px" >
-      
-      <form role="form" method="post" action="pages/email.html">
-        <fieldset>
- 
-        <label for="name"> Name<span class="asterik">*</span></label>
-        <input type="text" id="name" name="name">
-       
-        <br><br>
-
-        <label for="email"> Email<span class="asterik">*</span> </label>
-        <input type="text" id="email" name="email">
-
-        <br><br>
-
-        <label for="phone"> Phone<span class="asterik">*</span> </label>
-        <input type="text" id="phone" name="phone">
-
-        <br><br>
-      
-       <!--   <label for="city">City </label><br>
-      <input type="text" id="city" name="city"><br><br>
-
-         <label for="state"> State </label><br>
-      <input type="text" id="state" name="state"><br><br> -->
-
-      <label for="message" class="message">Message<span class="asterik">*</span> </label><br>    
-      <textarea rows="5" cols="50" name="message"></textarea><br><br>
-      <button name="submit" type="submit" class="btn btn-default">Submit</button>
-    </form>
-<!-- form -->
-  </div>
-  <!-- wrapper -->
-  <br>
-    </div>
-      <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12">
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-        consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-        cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-        proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-        consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-        cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-        proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+<div class="row">
+  <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+      <div class="map-responsive">
+         <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d6311.716152833115!2d-122.45514191580209!3d37.72300904228709!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x808f7e820309f599%3A0x4a970cf74aa2d80c!2s2275+San+Jose+Avenue%2C+San+Francisco%2C+CA+94112!5e0!3m2!1sen!2sus!4v1476931816874" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
       </div>
-  </div> <!-- end row -->
-</div>
-
-  <div class="map-responsive">
-   <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d6311.716152833115!2d-122.45514191580209!3d37.72300904228709!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x808f7e820309f599%3A0x4a970cf74aa2d80c!2s2275+San+Jose+Avenue%2C+San+Francisco%2C+CA+94112!5e0!3m2!1sen!2sus!4v1476931816874" width="600" height="450" frameborder="0" style="border:0" allowfullscreen>     
-   </iframe>
-  </div>
-
-<footer class='footer'>
-  <div class="container-fluid">
-    <div class = "icons">
-      <span class='socialMedia'> Follow us on &#8226 <span class='twitter'>Twitter</span> &#8226 <span class='facebook'>Facebook</span> &#8226 <span class='yelp'>Yelp</span> </span>
-      <a href=""><i class="fa fa-twitter"></i></a>
-      <a href="https://www.facebook.com/pages/Ajs-Bbq-And-Cafe/1671155576494952?ref=br_rs"><i class="fa fa-facebook"></i></a>
-      <a href="https://www.yelp.com/biz/ajs-bbq-and-cafe-san-francisco"><i class="fa fa-yelp"></i></a>
-    </div>
-  </div>
-</footer>
+  </div>  
+</div>  
 
     <script src="js/jquery2.0.min.js"></script>
     <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
