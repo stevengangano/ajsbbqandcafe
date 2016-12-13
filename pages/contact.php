e121338e5783ad84b7728996656ad4cc8dcf5882
Steven Gangano <stevengangano@Stevens-MacBook-Pro.local>
2016-12-12 21:07:03 -0800

    updated

--- pages/contact.php
+++ pages/contact.php
@@ -40,83 +40,71 @@
     </ul>
   </nav>
 </header>
-    
-<div class="row">
+  
+<div class="container-fluid">
+  <div class="row">
+    <div class="col-lg-8 col-md-8 col-sm-12 col-xs-12">
 
-  <div class="col-lg-8 col-md-8 col-sm-12 col-xs-12">
+      <h1> Contact </h1>
+      <hr class="hr">
 
-    <h1> Contact </h1>
+     <h4 class="featured-events-news-text">
+        AJ's BBQ and Cafe is located off the Geneva exit on US-101 at 2275 San Jose Ave, San Francisco, CA 94112. Parking in at AJ's BBQ and Cafe designated lot is available.
 
-    <hr class="hr">
+        <br>
+        <br>
+        <br>
 
-     <h6 class="featured-events-news-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-            quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-            consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-            cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-            proident, sunt in culpa qui officia deserunt mollit anim id est laborum. 
-      </h6>
-      <h6 class="featured-events-news-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-            quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-            consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-            cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-            proident, sunt in culpa qui officia deserunt mollit anim id est laborum. 
-      </h6>
-      <h6 class="featured-events-news-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-            quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-            consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-            cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-            proident, sunt in culpa qui officia deserunt mollit anim id est laborum. 
-      </h6>
-      <h6 class="featured-events-news-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-            quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-            consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-            cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-            proident, sunt in culpa qui officia deserunt mollit anim id est laborum. 
-      </h6>
-      <h6 class="featured-events-news-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-            quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-            consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-            cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-            proident, sunt in culpa qui officia deserunt mollit anim id est laborum. 
-      </h6>
-      <h6 class="featured-events-news-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-            quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-            consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-            cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-            proident, sunt in culpa qui officia deserunt mollit anim id est laborum. 
-      </h6>
-      <h6 class="featured-events-news-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-            quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-            consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-            cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-            proident, sunt in culpa qui officia deserunt mollit anim id est laborum. 
-      </h6>
+        Call us at <span class="phone-email">(650) 754-6891</span>
 
+        <br>
+        <br>
+        <br>
+         
+        For private event inquiries, please contact <span class="phone-email">ajsbbqandcafe@gmail.com.com</span>
 
+        <br>
+        <br>
+        <br>
+         
+        For inquiries, please contact:
+
+        <br>
+         
+        Kevin Guevarra | 650-644-4451
+        <span class="phone-email">ajsbbqandcafe@gmail.com.com</span>  
+
+        <br>
+        <br>
+        <br>
+         
+        Comments or questions? 
+
+        <br>
+        <br>
+        <br>
+
+        Contact us about your experience at <span class="phone-email"> ajsbbqandcafe@gmail.com</span>   
+    </h4>
   </div> <!-- end col -->
 
-    <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12">
-      <div class = "panel-body">
-        <img src="img/bbq.jpg" class="img-thumbnail about-photo">
+      <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12">
+        <div class = "panel-body">
+          <img src="img/bbq.jpg" class="img-thumbnail about-photo">
+        </div>
       </div>
-  </div>
 
-</div> <!-- end row -->
+    </div> <!-- end row -->
 
-<div class="row">
-  <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-      <div class="map-responsive">
-         <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d6311.716152833115!2d-122.45514191580209!3d37.72300904228709!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x808f7e820309f599%3A0x4a970cf74aa2d80c!2s2275+San+Jose+Avenue%2C+San+Francisco%2C+CA+94112!5e0!3m2!1sen!2sus!4v1476931816874" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
-      </div>
+  <div class="row">
+    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+        <div class="map-responsive">
+           <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d6311.716152833115!2d-122.45514191580209!3d37.72300904228709!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x808f7e820309f599%3A0x4a970cf74aa2d80c!2s2275+San+Jose+Avenue%2C+San+Francisco%2C+CA+94112!5e0!3m2!1sen!2sus!4v1476931816874" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+        </div>
+    </div>  
   </div>  
-</div>  
+
+</div> <!-- end container-fluid -->
 
     <script src="js/jquery2.0.min.js"></script>
     <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
