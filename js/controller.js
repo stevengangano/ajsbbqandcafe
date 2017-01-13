5f944abba283c7c678322c5d4615a7971d8a2cce
Steven Gangano <stevengangano@Stevens-MacBook-Pro.local>
2017-01-12 20:45:32 -0800

    updated

--- js/controller.js
+++ js/controller.js
@@ -7,39 +7,31 @@ myApp.config(function ($routeProvider) {
 	.when ('/', {
 		templateUrl: 'pages/home.html'	
 	})
-
 	.when ('/about', {
 		templateUrl: 'pages/about.html'
-		// controller: '2015videoGameCtrl'
 	})
 
 	.when ('/catering', {
 		templateUrl: 'pages/catering.html'
-		// controller: '2014videoGameCtrl'
 	})
 
 	.when ('/contact', {
 		templateUrl: 'pages/contact.php'
-		// controller: '2015videoGameCtrl'
 	})
 
 	.when ('/email', {
 		templateUrl: 'pages/email.html'
-		// controller: '2015videoGameCtrl'
 	})
 
 	.when ('/home', {
 		templateUrl: 'pages/home.html'
-		// controller: '2015videoGameCtrl'
 	})
 
 	.when ('/menu', {
 		templateUrl: 'pages/menu.html'
-		// controller: '2015videoGameCtrl'
 	})
 	.when ('/maps', {
 		templateUrl: 'pages/maps.html'
-		// controller: '2015videoGameCtrl'
 	})
 })
 
